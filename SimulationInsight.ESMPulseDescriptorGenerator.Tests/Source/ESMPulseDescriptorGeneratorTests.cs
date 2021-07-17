dc2294d1d874d61af972e3b659437fb26b1e13bc
Simulation Insight <simulationinsight@outlook.com>
2021-07-17 11:02:46 +0200

    Fixed breaking test.

--- SimulationInsight.ESMPulseDescriptorGenerator.Tests/Source/ESMPulseDescriptorGeneratorTests.cs
+++ SimulationInsight.ESMPulseDescriptorGenerator.Tests/Source/ESMPulseDescriptorGeneratorTests.cs
@@ -21,7 +21,7 @@ namespace SimulationInsight.ESMPulseDescriptorGenerator.Tests
                 PulseWidth = new Vector(1.0e-6, 2.0e-6, 11.0e-6),
                 FrequencyCentre = new Vector(9.1e9, 9.2e9, 9.16e9, 9.18e9),
                 FrequencyBandwidth = new Vector(10.0e6, 20.0e6),
-                PulseRepetitionFrequency = new Vector(1000.0, 990.0, 1100.0)
+                PulseRepetitionFrequency = new Vector(1000.0, 990.0, 1100.0),
             };
 
             var e = new ESMPulseDescriptorGenerator()
@@ -50,6 +50,7 @@ namespace SimulationInsight.ESMPulseDescriptorGenerator.Tests
                 EndTime = 50.0,
                 PulseWidth = new Vector(1.0e-6, 2.0e-6, 11.0e-6),
                 FrequencyCentre = new Vector(9.1e9, 9.2e9, 9.16e9, 9.18e9),
+                FrequencyBandwidth = new Vector(10.0e6, 20.0e6),
                 PulseRepetitionFrequency = new Vector(1000.0, 990.0, 1100.0)
             };
 
