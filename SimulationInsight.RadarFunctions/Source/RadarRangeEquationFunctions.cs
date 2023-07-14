4edbafc107c32ab00a599c4cf8c09abf93625963
Simulation Insight <simulationinsight@outlook.com>
2023-07-15 00:07:22 +0100

    Added RadarFunctions project.

---
+++ SimulationInsight.RadarFunctions/Source/RadarRangeEquationFunctions.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection.Metadata.Ecma335;
+using static System.Math;
+
+namespace SimulationInsight.RadarFunctions
+{
+    public static class RadarRangeEquationFunctions
+    {
+        public static double CalculateSignalPower(double transmitPower, double pulseWidth, double rfFrequency, double transmitGain, double receiveGain, double targetRange, double targetRCS, double systemLosses)
+        {
+            var rfWavelength = 3e8 / rfFrequency;
+
+            var numerator = transmitPower * pulseWidth * transmitGain * rfWavelength * rfWavelength * targetRCS;
+            var denominator = Pow((4 * PI), 3) * Pow(targetRange, 4) * systemLosses;
+
+            var signalPower = numerator / denominator;
+
+            return signalPower;
+        }
+    }
+}
