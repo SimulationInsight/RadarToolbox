8160f47ed052d7485954710ab694a4aebd028d6b
Simulation Insight <simulationinsight@outlook.com>
2023-07-14 23:49:08 +0100

    First version.

---
+++ SimulationInsight.RadarToolbox/SimulationInsight.MathLibrary/Source/Vector.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimulationInsight.MathLibrary
+{
+    public class Vector
+    {
+        public int NumberOfElements => Data.Length;
+
+        public double[] Data { get; set; }
+
+        public Vector(int numberOfElements)
+        { 
+            Data = new double[numberOfElements];
+        }
+
+        public Vector(params double[] data)
+        {
+            Data = data;
+        }
+
+        public double this[int index] 
+        { 
+            get => Data[index]; 
+            set => Data[index] = value; 
+        }
+    }
+}
