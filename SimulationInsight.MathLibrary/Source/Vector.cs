624aa627426b4f32422615db13654b40c37afb94
Simulation Insight <simulationinsight@outlook.com>
2023-07-15 23:39:31 +0100

    Added Matrix class.

--- SimulationInsight.MathLibrary/Source/Vector.cs
+++ SimulationInsight.MathLibrary/Source/Vector.cs
@@ -1,32 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace SimulationInsight.MathLibrary;
 
-namespace SimulationInsight.MathLibrary
+public class Vector
 {
-    public class Vector
+    public int NumberOfElements => Data.Length;
+
+    public double[] Data { get; set; }
+
+    public Vector(int numberOfElements)
+    { 
+        Data = new double[numberOfElements];
+    }
+
+    public Vector(params double[] data)
     {
-        public int NumberOfElements => Data.Length;
+        Data = data;
+    }
 
-        public double[] Data { get; set; }
-
-        public Vector(int numberOfElements)
-        { 
-            Data = new double[numberOfElements];
-        }
-
-        public Vector(params double[] data)
-        {
-            Data = data;
-        }
-
-        public double this[int index] 
-        { 
-            get => Data[index]; 
-            set => Data[index] = value; 
-        }
+    public double this[int index] 
+    { 
+        get => Data[index]; 
+        set => Data[index] = value; 
     }
 }
--- SimulationInsight.RadarFunctions/Source/RadarRangeEquationFunctions.cs
+++ SimulationInsight.RadarFunctions/Source/RadarRangeEquationFunctions.cs
@@ -1,20 +1,38 @@
-﻿using System.Reflection.Metadata.Ecma335;
-using static System.Math;
+﻿using static System.Math;
 
-namespace SimulationInsight.RadarFunctions
+namespace SimulationInsight.RadarFunctions;
+
+public static class RadarRangeEquationFunctions
 {
-    public static class RadarRangeEquationFunctions
+    public static double CalculateSignalPower(double transmitPower, double pulseWidth, double rfFrequency, double transmitGain, double receiveGain, double targetRange, double targetRCS, double systemLosses)
     {
-        public static double CalculateSignalPower(double transmitPower, double pulseWidth, double rfFrequency, double transmitGain, double receiveGain, double targetRange, double targetRCS, double systemLosses)
-        {
-            var rfWavelength = 3e8 / rfFrequency;
+        var rfWavelength = 3e8 / rfFrequency;
 
-            var numerator = transmitPower * pulseWidth * transmitGain * rfWavelength * rfWavelength * targetRCS;
-            var denominator = Pow((4 * PI), 3) * Pow(targetRange, 4) * systemLosses;
+        var numerator = transmitPower * pulseWidth * transmitGain * receiveGain * rfWavelength * rfWavelength * targetRCS;
+        var denominator = Pow((4 * PI), 3) * Pow(targetRange, 4) * systemLosses;
 
-            var signalPower = numerator / denominator;
+        var signalPower = numerator / denominator;
 
-            return signalPower;
-        }
+        return signalPower;
+    }
+
+    public static double CalculateNoiseTemperature(double noiseFigure)
+    {
+        var standardTemperature = 290.0;
+
+        var noiseTemperature = standardTemperature * noiseFigure;
+
+        return noiseTemperature;
+    }
+
+    public static double CalculateNoisePower(double noiseBandwidth, double noiseFigure)
+    {
+        var k = 1.0;
+
+        var standardTemperature = 290.0;
+
+        var noisePower = k * standardTemperature * noiseFigure * noiseBandwidth;
+
+        return noisePower;
     }
 }
