abd6209b0a8255f341c42820c20510d3c107592b
Simulation Insight <simulationinsight@outlook.com>
2023-07-16 20:18:12 +0100

    Added RadarDetectionModelHarness.

---
+++ SimulationInsight.RadarLibrary/Source/RadarDetectionModelData.cs
@@ -0,0 +1,8 @@
+﻿namespace SimulationInsight.RadarLibrary;
+
+public record RadarDetectionModelData
+{
+    public RadarDetectionModelInputs Inputs { get; set; }
+
+    public RadarDetectionModelOutputs Outputs { get; set; }
+}
---
+++ SimulationInsight.RadarLibrary/Source/RadarDetectionModelHarness.cs
@@ -0,0 +1,48 @@
+﻿namespace SimulationInsight.RadarLibrary;
+
+public class RadarDetectionModelHarness
+{
+    public RadarDetectionModelInputs DetectionModelInputs { get; set; }
+
+    public RadarDetectionModel DetectionModel { get; set; }
+
+    public double TargetRangeMin { get; set; }
+
+    public double TargetRangeMax { get; set; }
+
+    public double TargetRangeStep { get; set; }
+
+    public List<RadarDetectionModelData> DetectionModelData { get; set; }
+
+    public RadarDetectionModelHarness()
+    {
+    }
+
+    public void Run()
+    {
+        DetectionModelData = new List<RadarDetectionModelData>();
+
+        var range = TargetRangeMin;
+
+        DetectionModel = new RadarDetectionModel();
+
+        while (range <= TargetRangeMax)
+        {
+            var inputs = DetectionModelInputs with { TargetRange = range };
+
+            DetectionModel.Inputs = inputs;
+
+            DetectionModel.Run();
+
+            var data = new RadarDetectionModelData()
+            {
+                Inputs = DetectionModel.Inputs,
+                Outputs = DetectionModel.Outputs
+            };
+
+            DetectionModelData.Add(data);
+
+            range += TargetRangeStep;
+        }
+    }
+}
