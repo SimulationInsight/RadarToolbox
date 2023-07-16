70a07b8ef4ac75d15902a8cd1401521c117f3c8b
Simulation Insight <simulationinsight@outlook.com>
2023-07-16 20:45:56 +0100

    Added a new passing test and a new failing test. For checking Azure DevOps.

--- SimulationInsight.RadarLibrary.Tests/Source/WaveformParametersTests.cs
+++ SimulationInsight.RadarLibrary.Tests/Source/WaveformParametersTests.cs
@@ -22,4 +22,44 @@ public class WaveformParametersTests
         // Assert:
         Assert.AreEqual(w.PulseCompressionRatio, 500.0, 0.0001);
     }
+
+    [TestMethod]
+    public void CreateWaveformParameters2()
+    {
+        // Arrange:
+
+        // Act:
+        var w = new WaveformParameters()
+        {
+            WaveformName = "Waveform_1",
+            RfFrequencyCentre = 9.0e9,
+            PulseWidth = 10.0e-6,
+            PulseBandwidth = 50.0e6,
+            PulseRepetitionFrequency = 1000.0,
+            NumberOfPulses = 10
+        };
+
+        // Assert:
+        Assert.AreEqual(w.PulseCompressionRatio, 500.0, 0.0001);
+    }
+
+    [TestMethod]
+    public void CreateWaveformParameters3()
+    {
+        // Arrange:
+
+        // Act:
+        var w = new WaveformParameters()
+        {
+            WaveformName = "Waveform_1",
+            RfFrequencyCentre = 9.0e9,
+            PulseWidth = 10.0e-6,
+            PulseBandwidth = 50.0e6,
+            PulseRepetitionFrequency = 1000.0,
+            NumberOfPulses = 10
+        };
+
+        // Assert:
+        Assert.AreEqual(w.PulseCompressionRatio, 550.0, 0.0001);
+    }
 }
