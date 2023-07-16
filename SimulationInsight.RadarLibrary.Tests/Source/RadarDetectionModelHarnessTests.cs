3d5e6755215b0de6f549f0bc125ff0b9f357a30b
Simulation Insight <simulationinsight@outlook.com>
2023-07-16 20:25:23 +0100

    Added RadarLibrary tests project.

---
+++ SimulationInsight.RadarLibrary.Tests/Source/RadarDetectionModelHarnessTests.cs
@@ -0,0 +1,47 @@
+namespace SimulationInsight.RadarLibrary.Tests;
+
+[TestClass]
+public class RadarDetectionModelHarnessTests
+{
+    [TestMethod]
+    public void Test_1()
+    {
+        // Arrange:
+        var w = new WaveformParameters()
+        {
+            WaveformName = "Waveform_1",
+            RfFrequencyCentre = 9.0e9,
+            PulseWidth = 10.0e-6,
+            PulseBandwidth = 50.0e6,
+            PulseRepetitionFrequency = 1000.0,
+            NumberOfPulses = 10
+        };
+
+        var inputs = new RadarDetectionModelInputs()
+        {
+            RfSystemType = RfSystemType.MonostaticRadar,
+            WaveformParameters = w,
+            TransmitPeakPower = 500.0,
+            TransmitGain_dB = 32.0,
+            ReceiveGain_dB = 32.0,
+            ReceiverNoiseFigure_dB = 3.0,
+            SystemLosses_dB = 5.0,
+            TargetRange = 1.0,
+            TargetRadarCrossSection = 10.0
+        };
+
+        var harness = new RadarDetectionModelHarness()
+        {
+            DetectionModelInputs = inputs,
+            TargetRangeMin = 100.0,
+            TargetRangeMax = 100000.0,
+            TargetRangeStep = 100.0
+        };
+
+        // Act:
+        harness.Run();
+
+        // Assert:
+        Assert.AreEqual(harness.DetectionModelData.Count, 1000);
+    }
+}
---
+++ SimulationInsight.RadarLibrary.Tests/Source/WaveformParametersTests.cs
@@ -0,0 +1,25 @@
+namespace SimulationInsight.RadarLibrary.Tests;
+
+[TestClass]
+public class WaveformParametersTests
+{
+    [TestMethod]
+    public void CreateWaveformParameters()
+    {
+        // Arrange:
+
+        // Act:
+        var w = new WaveformParameters()
+        {
+            WaveformName = "Waveform_1",
+            RfFrequencyCentre = 9.0e9,
+            PulseWidth = 10.0e-6,
+            PulseBandwidth = 50.0e6,
+            PulseRepetitionFrequency = 1000.0,
+            NumberOfPulses = 10
+        };
+
+        // Assert:
+        Assert.AreEqual(w.PulseCompressionRatio, 500.0, 0.0001);
+    }
+}
